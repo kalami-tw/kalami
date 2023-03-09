4e1bacb267e4e7c2c13c66b7a598f0ab866bdd19
PhilChen <phil.chen@taikoomotors.com>
2023-03-10 00:42:26 +0800

    test003

--- src/main/java/com/example/kalami/KalamiApplication.java
+++ src/main/java/com/example/kalami/KalamiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KalamiApplication {
-    //test123
+    //test123456
 
     public static void main(String[] args) {
         SpringApplication.run(KalamiApplication.class, args);
