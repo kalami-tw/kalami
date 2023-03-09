72ce379bc464325a62a9ca3983fbc987a75f372c
PhilChen <phil.chen@taikoomotors.com>
2023-03-10 00:09:15 +0800

    1st commit

---
+++ src/main/java/com/example/kalami/KalamiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kalami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalamiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KalamiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kalami/KalamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kalami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalamiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
